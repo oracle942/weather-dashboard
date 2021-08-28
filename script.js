798e8a5e93cfb370946e5d4894700676b36b4510
Gerardo Sanchez <oracle942@gmail.com>
2021-08-28 13:43:38 -0700

    created algorithm to statically display weather data on screen

--- script.js
+++ script.js
@@ -2,10 +2,13 @@
 var APIKey = "e435637a5f14a87e81f5614e146cda4a";
 var userInput = document.querySelector(".user-input")
 var searchBtn = document.querySelector("#search-button")
-
+var tempVal = document.querySelector(".temp")
+var windVal = document.querySelector(".wind")
+var humVal = document.querySelector(".humidity")
+var uvVal = document.querySelector(".uv")
 
 function getApi() {
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=e435637a5f14a87e81f5614e146cda4a&units=imperial';
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=houston&appid=e435637a5f14a87e81f5614e146cda4a&units=imperial';
   
     fetch(requestUrl)
       
@@ -16,11 +19,15 @@ function getApi() {
     .then(function (data) {
         
               
-        var temp = data.main.temp;
-                   
+        var tempData = data.main.temp;
+        var humData = data.main.humidity;
+        var windData = data.wind.speed;
+        tempVal.textContent = tempData        
+        humVal.textContent = humData        
+        windVal.textContent = windData        
           
         console.log(data)
-        console.log(x)
+        console.log(tempData)
   
       });
   }
