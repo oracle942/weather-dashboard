e72a55ec85b082c7c2c7542bd87a94d2b9918a5d
Gerardo Sanchez <oracle942@gmail.com>
2021-08-28 12:44:11 -0700

    added API key. changed API units. created data and temp consolelogs

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var searchBtn = document.querySelector("#search-button")
 
 
 function getApi() {
-    var requestUrl = 'https://api.github.com/users/oracle942/repos';
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=e435637a5f14a87e81f5614e146cda4a&units=imperial';
   
     fetch(requestUrl)
       
@@ -14,13 +14,14 @@ function getApi() {
       })
       
     .then(function (data) {
+        
+              
+        var temp = data.main.temp;
+                   
+          
         console.log(data)
-        // for (var i = 0; i < data.length; i++) {
-        //     var listItem = document.createElement("h1");
-        //     listItem.textContent = data[i].name
-        //     div1.appendChild(listItem)
-
-        //   }
+        console.log(x)
+  
       });
   }
 
