3bfdc3f26b1706379143eacf909d1ffb5c0b0076
Gerardo Sanchez <oracle942@gmail.com>
2021-08-28 00:11:42 -0700

    Finished html scaffolding. Added API key.

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
 // var div1 = document.querySelector(".div1")
+var APIKey = "e435637a5f14a87e81f5614e146cda4a";
 
 
-// function getApi() {
-//     var requestUrl = 'https://api.github.com/users/oracle942/repos';
+function getApi() {
+    var requestUrl = 'https://api.github.com/users/oracle942/repos';
   
-//     fetch(requestUrl)
+    fetch(requestUrl)
       
-//     .then(function (response) {
-//         return response.json();
-//       })
+    .then(function (response) {
+        return response.json();
+      })
       
-//     .then(function (data) {
-//         console.log(data)
-//         for (var i = 0; i < data.length; i++) {
-//             var listItem = document.createElement("h1");
-//             listItem.textContent = data[i].name
-//             div1.appendChild(listItem)
+    .then(function (data) {
+        console.log(data)
+        // for (var i = 0; i < data.length; i++) {
+        //     var listItem = document.createElement("h1");
+        //     listItem.textContent = data[i].name
+        //     div1.appendChild(listItem)
 
-//           }
-//       });
-//   }
-//   getApi()
+        //   }
+      });
+  }
+  getApi()
   
+
+//   API key e435637a5f14a87e81f5614e146cda4a
