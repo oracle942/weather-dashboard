6b65b19f112a3b6030b6d7a77e8b1aa0badcef40
Gerardo Sanchez <oracle942@gmail.com>
2021-08-29 00:55:47 -0700

    Completed 5-day-forecast weather display. Added query-selector variables for city buttons.

--- script.js
+++ script.js
@@ -2,6 +2,15 @@
 var APIKey = "e435637a5f14a87e81f5614e146cda4a";
 var userInput = document.querySelector(".user-input")
 var searchBtn = document.querySelector("#search-button")
+var searchBtn = document.querySelector("#Austin")
+var searchBtn = document.querySelector("#Chicago")
+var searchBtn = document.querySelector("#New-York")
+var searchBtn = document.querySelector("#Orlando")
+var searchBtn = document.querySelector("#San-Francisco")
+var searchBtn = document.querySelector("#Seattle")
+var searchBtn = document.querySelector("#Denver")
+var searchBtn = document.querySelector("#Atlanta")
+var searchBtn = document.querySelector("#Atlanta")
 var tempVal = document.querySelector(".temp")
 var windVal = document.querySelector(".wind")
 var humVal = document.querySelector(".humidity")
@@ -20,11 +29,24 @@ var temp1 = document.querySelector(".temp1")
 var temp2 = document.querySelector(".temp2")
 var temp3 = document.querySelector(".temp3")
 var temp4 = document.querySelector(".temp4")
+var temp5 = document.querySelector(".temp5")
+var wind1 = document.querySelector(".wind1")
+var wind2 = document.querySelector(".wind2")
+var wind3 = document.querySelector(".wind3")
+var wind4 = document.querySelector(".wind4")
+var wind5 = document.querySelector(".wind5")
+var hum1 = document.querySelector(".hum1")
+var hum2 = document.querySelector(".hum2")
+var hum3 = document.querySelector(".hum3")
+var hum4 = document.querySelector(".hum4")
+var hum5 = document.querySelector(".hum5")
 
 
-var icn = []
+var icnArr = []
 var dateArr = []
 var tempArr = []
+var windArr = []
+var humArr = []
 var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=houston&appid=e435637a5f14a87e81f5614e146cda4a&units=imperial';
 var requestUrl5df = 'https://api.openweathermap.org/data/2.5/forecast?q=orlando&appid=e435637a5f14a87e81f5614e146cda4a&units=imperial';
 
@@ -65,6 +87,8 @@ function getApi1() {
                 var y = x.split(' ')
 
                 var tempData = data.list[i].main.temp
+                var windData = data.list[i].wind.speed
+                var humData = data.list[i].main.humidity
             //    console.log(tempData)
 
                         if(y[1] === "12:00:00"){
@@ -76,6 +100,14 @@ function getApi1() {
                             const temp = document.createElement('p')
                             temp.textContent = tempData
                             tempArr.push(temp)
+
+                            const wind = document.createElement('p')
+                            wind.textContent = windData
+                            windArr.push(wind)
+
+                            const hum = document.createElement('p')
+                            hum.textContent = humData
+                            humArr.push(hum)
                             
 
 
@@ -83,32 +115,45 @@ function getApi1() {
                             var icon = document.createAttribute('src')
                             icon.value = 'http://openweathermap.org/img/wn/'+data.list[i].weather[0].icon+'@2x.png'
                             img.setAttribute('src', icon)
-                            icn.push(icon.value)
+                            icnArr.push(icon.value)
+                            
 
 
                            
                         } 
                 }
-                            
-                console.log(tempArr)
+                console.log(humArr[0])      
+                
                 date1.textContent = dateArr[0].innerText                 
                 date3.textContent = dateArr[1].innerText                 
                 date2.textContent = dateArr[2].innerText                 
                 date4.textContent = dateArr[3].innerText                 
                 date5.textContent = dateArr[4].innerText 
                 
-                temp1.innerText = "Temp: " + tempArr[0].innerText
-                temp2.innerText = "Temp: " + tempArr[1].innerText
+                temp1.textContent = "Temp: " + tempArr[0].innerText
+                temp2.textContent = "Temp: " + tempArr[1].innerText
                 temp3.textContent = "Temp: " + tempArr[2].innerText
                 temp4.textContent = "Temp: " + tempArr[3].innerText
                 temp5.textContent = "Temp: " + tempArr[4].innerText
                    
+                wind1.textContent = "Wind: " + windArr[0].innerText
+                wind2.textContent = "Wind: " + windArr[1].innerText
+                wind3.textContent = "Wind: " + windArr[2].innerText
+                wind4.textContent = "Wind: " + windArr[3].innerText
+                wind5.textContent = "Wind: " + windArr[4].innerText
+                   
+                hum1.textContent = "Humidity: " + humArr[0].innerText
+                hum2.textContent = "Humidity: " + humArr[1].innerText
+                hum3.textContent = "Humidity: " + humArr[2].innerText
+                hum4.textContent = "Humidity: " + humArr[3].innerText
+                hum5.textContent = "Humidity: " + humArr[4].innerText
+                   
 
-                img1.setAttribute('src', icn[0])
-                img2.setAttribute('src', icn[1])
-                img3.setAttribute('src', icn[2])
-                img4.setAttribute('src', icn[3])
-                img5.setAttribute('src', icn[4])
+                img1.setAttribute('src', icnArr[0])
+                img2.setAttribute('src', icnArr[1])
+                img3.setAttribute('src', icnArr[2])
+                img4.setAttribute('src', icnArr[3])
+                img5.setAttribute('src', icnArr[4])
                 
               
                   })
@@ -117,8 +162,8 @@ function getApi1() {
               
                    
               
-                  // searchBtn.addEventListener("click", function(){
-                  // })
+                  searchBtn.addEventListener("click", function(){
+                  })
                   
               
               
