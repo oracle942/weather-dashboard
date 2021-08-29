94b29273ff16060d1f339fa8a0b34e97a7b0e630
Gerardo Sanchez <oracle942@gmail.com>
2021-08-28 18:15:05 -0700

    working on algorithm to populate 5-day-forecast search results

--- script.js
+++ script.js
@@ -6,49 +6,65 @@ var tempVal = document.querySelector(".temp")
 var windVal = document.querySelector(".wind")
 var humVal = document.querySelector(".humidity")
 var uvVal = document.querySelector(".uv")
+var date1 = document.getElementById("date1")
+var arr = []
 var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=houston&appid=e435637a5f14a87e81f5614e146cda4a&units=imperial';
 var requestUrl5df = 'https://api.openweathermap.org/data/2.5/forecast?q=dallas&appid=e435637a5f14a87e81f5614e146cda4a&units=imperial';
-
-
 function getApi() {
   
     fetch(requestUrl5df)
       
-    .then(function (response) {
-        return response.json();
-      })
+        .then(function (response) {
+            return response.json();
+        })
       
-    .then(function (data) {
-        console.log(data)
-        // console.log(data.list[0].dt_txt)
-              
-        // var tempData = data.main.temp;
-        // var humData = data.main.humidity;
-        // var windData = data.wind.speed;
-        // tempVal.textContent = tempData        
-        // humVal.textContent = humData        
-        // windVal.textContent = windData        
-        // 
-        for(var i = 0; i < data.list.length; i++){
-        var x = data.list[i].dt_txt
-        var y = x.split(' ')
-        // console.log(x)
-        // console.log(y)
+            .then(function (data) {
+                console.log(data)
+                // console.log(data.list[0].dt_txt)
+                    
+                // var tempData = data.main.temp;
+                // var humData = data.main.humidity;
+                // var windData = data.wind.speed;
+                // tempVal.textContent = tempData        
+                // humVal.textContent = humData        
+                // windVal.textContent = windData        
+                // 
+                for(var i = 0; i < data.list.length; i++){
+                var arr = []
+                var x = data.list[i].dt_txt
+                var y = x.split(' ')
+                // console.log(x)
+                // console.log(y)
 
-        if(y[1] === "00:00:00"){
+                        if(y[1] === "12:00:00"){
+                   
+                            var icon = document.createAttribute('src')
+                            icon.value = 'http://openweathermap.org/img/wn/'+data.list[i].weather[0].icon+'@2x.png'
+                            // date1.setAttributeNode(icon)
+                            arr.push(icon.value)
+                            // console.log(arr)
+                    
+                           
+                            
 
-        console.log(y[0])
-        // console.log(data.list[i].dt_txt)
-    }
-    
+                            
+                                
+                           
+                            
+                        
+                        // console.log(data.list[i].weather[0].icon)
 
-        } 
-    })
+                        // console.log(data.list[i].dt_txt)
+                //     }
+                    
+                } 
+
+            }
   
+console.log(arr)
+    })
 
 }
-
-  
 
     // var xhr = new XMLHttpRequest("https://api.openweathermap.org/data/2.5/forecast?q=dallas&appid=e435637a5f14a87e81f5614e146cda4a&units=imperial");
     // xhr.onreadystatechange = function () {
