f099972a072fb28681dd766306c0d4a01018558a
Gerardo Sanchez <oracle942@gmail.com>
2021-08-28 19:20:00 -0700

    added array to store API call results

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ var windVal = document.querySelector(".wind")
 var humVal = document.querySelector(".humidity")
 var uvVal = document.querySelector(".uv")
 var date1 = document.getElementById("date1")
+var date2 = document.getElementById("date2")
+var date3 = document.getElementById("date3")
+var date4 = document.getElementById("date4")
+var date5 = document.getElementById("date5")
+
+
 var arr = []
 var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=houston&appid=e435637a5f14a87e81f5614e146cda4a&units=imperial';
 var requestUrl5df = 'https://api.openweathermap.org/data/2.5/forecast?q=dallas&appid=e435637a5f14a87e81f5614e146cda4a&units=imperial';
@@ -30,38 +36,35 @@ function getApi() {
                 // windVal.textContent = windData        
                 // 
                 for(var i = 0; i < data.list.length; i++){
-                var arr = []
                 var x = data.list[i].dt_txt
                 var y = x.split(' ')
                 // console.log(x)
                 // console.log(y)
 
                         if(y[1] === "12:00:00"){
-                   
+        
                             var icon = document.createAttribute('src')
                             icon.value = 'http://openweathermap.org/img/wn/'+data.list[i].weather[0].icon+'@2x.png'
-                            // date1.setAttributeNode(icon)
-                            arr.push(icon.value)
-                            // console.log(arr)
+                            arr.push(icon)
+                            console.log(arr.length)
+
+                            // for(var j = 0; j <
+                            date1.setAttribute('src', icon.value)
+                            
+                            
+                        } 
+                }
+                            
+                            
                     
                            
                             
 
                             
                                 
-                           
-                            
-                        
-                        // console.log(data.list[i].weather[0].icon)
 
-                        // console.log(data.list[i].dt_txt)
-                //     }
-                    
-                } 
-
-            }
   
-console.log(arr)
+
     })
 
 }
